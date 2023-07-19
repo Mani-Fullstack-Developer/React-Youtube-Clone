d5fea4aa2808a2fabd139f3a185e1d107a61c7de
Mani-Fullstack-Developer <manifullstackdeveloper@gmail.com>
2023-07-19 14:39:48 +0530

    React youtube clone

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": "b0122970b6mshc97e64ce23a37c3p115985jsncb05acd8e746",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+export const fetchDataFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
